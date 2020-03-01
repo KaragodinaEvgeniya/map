06b728d0a97a7056e73d37083c770565af5d5957
Evgenya <ginesing@mail.ru>
2020-03-02 00:07:04 +0300

    localStorageProblem

--- src/assets/scripts/welcome.js
+++ src/assets/scripts/welcome.js
@@ -1,12 +1,14 @@
-
 // eslint-disable-next-line no-undef
 ymaps.ready(init);
-
-
-
 var modal = document.getElementById('balloon');
 let closeModal = document.getElementById('close');
 var feedbacksArray = [];
+var inputName = document.getElementsByTagName('name');
+var inputPlace = document.getElementsByTagName('place');
+var inputText = document.getElementsByTagName('comment');
+var addButton = document.getElementById('btn')[document.getElementById('btn').length - 1];
+var myInput = document.getElementById('address');
+// let comments = localStorage.getItem('comments') ? JSON.parse(localStorage.getItem('comments') ) : [];
 function init() {
     // eslint-disable-next-line no-undef
     var myMap = new ymaps.Map('map', {
@@ -18,7 +20,6 @@ function init() {
         searchControlProvider: 'yandex#search',
         geoObjectOpenBalloonOnClick: false
     });
-
     // eslint-disable-next-line no-undef
     var clusterer = new ymaps.Clusterer({
         preset: 'islands#invertedDarkOrangeClusterIcons',
@@ -27,121 +28,109 @@ function init() {
         groupByCoordinates: false,
         clusterBalloonContentLayout: 'cluster#balloonCarousel'
     });
-
     myMap.geoObjects.add(clusterer);
-
     myMap.events.add('click', function(e) {
         var coords = e.get('coords');
+
+        // eslint-disable-next-line no-undef
+        addFeedback(coords);
+    });
+    function addFeedback(coords){
         // eslint-disable-next-line no-undef
         var geoCoords = ymaps.geocode(coords);
-        var position = e.get('position');
+        // var position = e.get('position');
         modal.classList.add('balloon--active');
+
         geoCoords.then(res => {
             var obj = {};
-
             obj.coords = coords;
             obj.address = res.geoObjects.get(0).properties.get('text');
             obj.comments = [];
 
-            if (position[1] > window.screen.availHeight - 526) {
-                position[1] = window.screen.availHeight - 526;
-            }
-            if (position[0] > window.screen.availWidth - 380) {
-                position[0] = window.screen.availWidth - 760;
-            }
+            myInput.innerHTML = obj.address;
+            var feedbacks = document.getElementById('balloon-list');
+            var feedback = document.createElement('li');
+            feedback.classList.add('feedback');
+            // eslint-disable-next-line no-undef
+            feedback.innerHTML = hintContent;
+            feedbacks.appendChild(feedback);
 
-            modal(obj, myMap, position, clusterer, '');
+
+            addButton.addEventListener('click', (e) => {
+                e.preventDefault();
+                if (inputName.value && inputPlace.value && inputText.value) {
+                    // var form = document.getElementById('my-form');
+                    var feedback = document.createElement('li');
+                    var name = document.createElement('div');
+                    var place = document.createElement('div');
+                    var text = document.createElement('div');
+                    var day = document.createElement('div');
+                    var firstLine = document.createElement('div');
+                    name.innerHTML = JSON.parse(localStorage.getItem('name') );
+                    place.innerHTML = JSON.parse(localStorage.getItem('place') );
+                    text.innerHTML = JSON.parse(localStorage.getItem('textarea') );
+                    var date = new Date();
+                    day.innerHTML = date.getDate() + '.' + date.getMonth() + '.' + date.getFullYear();
+                    feedback.classList.add('feedback');
+                    name.classList.add('feedback__name');
+                    place.classList.add('feedback__place');
+                    text.classList.add('feedback__text');
+                    firstLine.classList.add('feedback__firstLine');
+                    firstLine.appendChild(name);
+                    firstLine.appendChild(place);
+                    firstLine.appendChild(day);
+                    feedback.appendChild(firstLine);
+                    feedback.appendChild(text);
+                    feedbacks.appendChild(feedback);
+                    // inputName.value = JSON.parse(localStorage.getItem('name') );
+                    // inputPlace.value = JSON.parse(localStorage.getItem('place') );
+                    // inputText.value = JSON.parse(localStorage.getItem('textarea') );
+
+                        localStorage.setItem("name", JSON.stringify(document.getElementById("name")) .value);
+                        localStorage.setItem("place", JSON.stringify(document.getElementById("place")) .value);
+                        localStorage.setItem("textarea", JSON.stringify(document.getElementById("textarea")) .value);
+                    // comments = form.elements.value;
+
+
+
+                    feedbacksArray.push(feedback);
+                } else {
+                    alert('Заполните все поля!')
+                }
+
+                var myPlacemark = placemarks(coords);
+                myMap.geoObjects.add(myPlacemark);
+
+            })
         });
-    });
-
-    function openModal(obj, myMap, position, clusterer, hintContent) {
-        addFeedback(obj, myMap, position, clusterer, modal, hintContent);
     }
 
-    function addFeedback(obj, myMap, position, clusterer, modal, hintContent){
-        var inputName = document.getElementsByTagName('name');
-        var inputPlace = document.getElementsByTagName('place');
-        var inputText = document.getElementsByTagName('comment');
-        var addButton = document.getElementById('btn')[document.getElementById('btn').length - 1];
-        var myInput = document.getElementById('address');
-
-        myInput.innerHTML = obj.address;
-
-        var feedbacks = document.getElementById('balloon-list');
-        var feedback = document.createElement('li');
-
-        feedback.classList.add('feedback');
-        feedback.innerHTML = hintContent;
-        feedbacks.appendChild(feedback);
-
-        addButton.addEventListener('click', (p) => {
-            p.preventDefault();
-            if (inputName.value && inputPlace.value && inputText.value) {
-                var feedback = document.createElement('li');
-
-                var name = document.createElement('div');
-                var place = document.createElement('div');
-                var text = document.createElement('div');
-                var day = document.createElement('div');
-                var firstLine = document.createElement('div');
-
-                name.innerHTML = inputName.value;
-                place.innerHTML = inputPlace.value;
-                text.innerHTML = inputText.value;
-
-                var date = new Date();
-
-                day.innerHTML = date.getDate() + '.' + date.getMonth() + '.' + date.getFullYear();
-
-                feedback.classList.add('feedback');
-                name.classList.add('feedback__name');
-                place.classList.add('feedback__place');
-                text.classList.add('feedback__text');
-                firstLine.classList.add('feedback__firstLine');
-
-                firstLine.appendChild(name);
-                firstLine.appendChild(place);
-                firstLine.appendChild(day);
-
-                feedback.appendChild(firstLine);
-                feedback.appendChild(text);
-
-                feedbacks.appendChild(feedback);
-
-                inputName.value = '';
-                inputPlace.value = '';
-                inputText.value = '';
-
-                placemarks(obj, myMap, position, clusterer, modal);
-                feedbacksArray.push(feedback);
-            } else {
-                alert('Заполните все поля!')
-            }
-        })
-    }
-
-    function placemarks(obj, myMap, position, clusterer, modal) {
+    function placemarks(coords) {
         // eslint-disable-next-line no-undef
-        var placemark = new ymaps.Placemark(obj.coords, {
-            hintContent: modal.children[1].lastChild.innerHTML,
-            modalContent: obj.address + modal.children[1].lastChild.innerHTML
-        }, {
+        return new ymaps.Placemark(coords, {
             preset: 'islands#darkOrangeDotIcon',
-            openHintOnHover: false
+            draggable: true
         });
-
-        myMap.geoObjects.add(placemark);
-        clusterer.add(placemark);
-
-        placemark.events.add('click', () => {
-            openModal(obj, myMap, position, clusterer, placemark.properties._data.hintContent);
-        })
     }
 
-
+    // function placemarks(obj, myMap, position, clusterer, modal) {
+    //     // eslint-disable-next-line no-undef
+    //     var placemark = new ymaps.Placemark(obj.coords, {
+    //         hintContent: modal.children[1].lastChild.innerHTML,
+    //         modalContent: obj.address + modal.children[1].lastChild.innerHTML
+    //     }, {
+    //         preset: 'islands#darkOrangeDotIcon',
+    //         openHintOnHover: false
+    //     });
+    //     myMap.geoObjects.add(placemark);
+    //     clusterer.add(placemark);
+    //     placemark.events.add('click', () => {
+    //         // eslint-disable-next-line no-undef
+    //         openModal(obj, myMap, position, clusterer, placemark.properties._data.hintContent);
+    //     })
+    // }
     closeModal.addEventListener('click', function(){
         modal.classList.remove('balloon--active');
         modal.innerHTML = '';
     });
-
 }
